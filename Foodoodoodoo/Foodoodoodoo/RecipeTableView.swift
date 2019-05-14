2ec3ee6161eeb79a16a4b55a56c5ae53f7e4e269
Min Woo <minwoo@iMac5.local>
2019-05-14 11:16:52 -0400

    sample recipe shows, need functions

--- Foodoodoodoo/Foodoodoodoo/RecipeTableView.swift
+++ Foodoodoodoo/Foodoodoodoo/RecipeTableView.swift
@@ -11,18 +11,55 @@ import UIKit
 class RecipeTableView: UITableViewController {
     
     var foods = [Food]()
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foods.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FoodCell", for: indexPath)
+        
+        cell.textLabel?.text = foods[indexPath.row].name
+        cell.detailTextLabel?.text = foods[indexPath.row].detailDescription
+        
+        return cell
+    }
+    @IBAction func editButtonTapped(_ sender: UIBarButtonItem) {
+        let tableViewEditingMode = tableView.isEditing
+        
+        tableView.setEditing(!tableViewEditingMode, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            foods.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+        let movedFood = foods.remove(at: fromIndexPath.row)
+        foods.insert(movedFood, at: to.row)
+        tableView.reloadData()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
         foods = Food.loadSampleRecipe()
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44.0
-        // Do any additional setup after loading the view.
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 4
-    }
+
     
 
     /*
